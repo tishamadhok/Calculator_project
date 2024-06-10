6e8e50070c8667790a4426fb5cc968086b6f012e
Tisha Madhok <tishamadhok@Tishas-MacBook-Pro.local>
2024-06-10 16:22:25 -0400

    Add main.py and conftest.py with Faker and test data generation

--- tests/test_calculator.py
+++ tests/test_calculator.py
@@ -1,19 +1,26 @@
 import pytest
 from calculator.calculator import Calculator
 
-def test_add():
-    assert Calculator.add(1, 1) == 2
+def test_add(fake_data):
+    a, b, operation, expected_result = fake_data
+    if operation == 'add':
+        assert Calculator.add(a, b) == expected_result
 
-def test_subtract():
-    assert Calculator.subtract(2, 1) == 1
+def test_subtract(fake_data):
+    a, b, operation, expected_result = fake_data
+    if operation == 'subtract':
+        assert Calculator.subtract(a, b) == expected_result
 
-def test_multiply():
-    assert Calculator.multiply(2, 3) == 6
+def test_multiply(fake_data):
+    a, b, operation, expected_result = fake_data
+    if operation == 'multiply':
+        assert Calculator.multiply(a, b) == expected_result
 
-def test_divide():
-    assert Calculator.divide(10, 2) == 5
+def test_divide(fake_data):
+    a, b, operation, expected_result = fake_data
+    if operation == 'divide' and b != 0:
+        assert Calculator.divide(a, b) == expected_result
 
 def test_divide_by_zero():
     with pytest.raises(ValueError):
         Calculator.divide(10, 0)
-        
